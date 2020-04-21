de465a3ebd70a86eb3f97308cecaaa6d4b077ad2
Sakorni <ssakorni@gmail.com>
2020-04-21 22:07:39 +0300

    Finished the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/agression.jpg'),
+              ),
+              Text(
+                "Danila Scherbina",
+                style: TextStyle(
+                    fontFamily: "Pacifico",
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "FUCKING  BOT",
+                style: TextStyle(
+                    fontFamily: "SourceSans",
+                    fontSize: 25.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
